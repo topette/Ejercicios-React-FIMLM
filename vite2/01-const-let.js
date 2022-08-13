cf47bca8fbdec66c84248c1ac3d801d5679d3e5d
Leonardo Osorio <topette@users.noreply.github.com>
2022-08-13 10:04:15 -0500

    ejercicios con js

---
+++ vite2/01-const-let.js
@@ -0,0 +1,31 @@
+//var // ya no se usa
+
+//let // esta variable puede cambiar
+
+//const // constante
+
+// scope global
+//let numero = 0
+
+if (true){
+    //scope local (ambito)
+    let nombre = "pedro"
+    let numero = 1
+    console.log(nombre)
+}
+
+
+
+let nombre = "juan"
+console.log(nombre)
+
+// objeto literal ahora es el que se usa
+const miNombre = "Leonardo"
+const nombre2 = `Hola soy ${miNombre}`
+const nombre3 = `Hola soy ${ () => { } }`
+console.log(nombre2)
+
+
+// En react
+onClick={}
+
---
+++ vite2/lorem.js
@@ -0,0 +1,6 @@
+const titulo = "Noticia 13 agosto";
+console.log(titulo);
+
+const parrafo = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+const parrafoCortado = parrafo.slice(0, 30);
+console.log(parrafoCortado);
